58f1c0167708179a1b6e8e9adde3d023cc34f5c6
SurathuR <99076785+SurathuR@users.noreply.github.com>
2026-08-11 22:10:48 +0530

    second

--- roboshop.sh
+++ roboshop.sh
@@ -8,9 +8,9 @@ for instance in $@
 do
     echo "Launching instance: $instance"
     INSTANCE_ID=$(aws ec2 run-instances \
-        --image-id ami-0220d79f3f480ecf5 \
+        --image-id $AMI_ID \
         --instance-type t3.micro \
-        --security-groups "Roboshop-SSH" "Roboshop-$instance" \
+        --security-groups "Shell-Scripting" "Roboshop-$instance" \
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
         --query 'Instances[0].InstanceId' \
         --output text
